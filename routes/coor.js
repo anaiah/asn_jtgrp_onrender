bb49200d03df71b217342104792456808ef81251
anaiah <carlodominguez@yahoo.com>
2025-05-08 18:15:59 +0800

    alam

--- routes/coor.js
+++ routes/coor.js
@@ -35,11 +35,24 @@ connectDb()
 });  
 //=================================START HERE ============================
 
+const getmos = () => {
+    var series = new Date() 
+	var mm = String( series.getMonth() + 1).padStart(2, '0') //January is 0!
+	var yyyy = series.getFullYear()
+
+	series = yyyy+'-'+mm 
+
+    return series
+}
+
+
 //==========SUMMARY OF COORDS
 router.get('/summary', async(req,res)=>{
     connectDb()
     .then((db)=>{ 
 
+        const xmos = getmos()
+        console.log('firing summary()====')
         sql2 =`select 
                 c.coordinator,
                 c.location,
@@ -67,12 +80,31 @@ router.get('/summary', async(req,res)=>{
                     join asn_users y 
                     on x.emp_id = y.id 
                     where  y.hub = c.hub and x.created_at like '2025-05%' group by y.hub
-                ) as amount_remitted
+                ) as amount_remitted,
+
+                
+                (
+                    round(concat(
+                    ( select  sum(x.actual_parcel) 
+                    from asn_transaction x 
+                        join asn_users y 
+                        on x.emp_id = y.id 
+                        where  y.hub = c.hub and x.created_at like '2025-05%' group by y.hub
+                    ) /
+                    ( select  sum(x.parcel) 
+                    from asn_transaction x 
+                    join asn_users y 
+                    on x.emp_id = y.id 
+                    where  y.hub = c.hub and x.created_at like '${xmos}%' group by y.hub
+                    ) * 100
+                    ,'%'),0)
+                ) as qty_pct
+
                 from asn_hub  c 
                 left join 
                 asn_users b ON
                 c.hub = b.hub
-                where c.coordinator  like '%salimbag%'
+                where c.coordinator_email = 'salimbagat.mcgold24@gmail.com'
                 group by c.hub
                 order by c.location;`
             
@@ -84,6 +116,49 @@ router.get('/summary', async(req,res)=>{
             closeDb( db )
             
             //console.log(  results) 
+            res.status(200).send(results )
+
+        })
+
+    }).catch((error)=>{
+        res.status(500).json({error:'x'})
+    }) 
+})
+
+
+//===============syummary riders
+router.get('/ridersummary/:hub', async(req,res)=>{
+    connectDb()
+    .then((db)=>{ 
+        console.log('firing rider-summary()====')
+        
+        const xmos = getmos()
+
+        sql2 =`select b.id,a.full_name, 
+            count(emp_id) as transactions,
+            b.emp_id, a.hub,
+            sum(b.parcel) as qty,
+            sum(b.actual_parcel) as actual_qty,
+            round(sum(b.amount),2) as amt,
+            round(sum(b.actual_amount),2) as actual_amt,
+            round((sum(b.actual_parcel)/sum(b.parcel))*100) as delivered_pct,
+            round(100-(sum(b.actual_parcel)/sum(b.parcel))*100) as undelivered_pct
+            from asn_users a 
+            left join asn_transaction b
+            on  a.id = b.emp_id
+            where b.created_at like '${xmos}%' and a.grp_id =1 and upper(a.hub) = '${req.params.hub}'
+            group by b.emp_id 
+            order by (sum(b.actual_parcel)/sum(b.parcel))*100 desc;
+`
+            
+        //console.log(sql)
+        console.log(sql2,)
+
+        db.query( sql2 , null , (error, results)=>{
+            
+            closeDb( db )
+            
+            console.log(  results) 
             res.status(200).send(results )
 
         })
