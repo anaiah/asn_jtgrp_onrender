9cf110c163a097712e9c28f46fbcca9938daed69
anaiah <carlodominguez@yahoo.com>
2026-09-04 17:51:10 +0800

    Update qrjhuang.js

--- routes/qrjhuang.js
+++ routes/qrjhuang.js
@@ -268,7 +268,7 @@ router.post('/qrxls' , upload.single('hris_upload_file'), async (req, res) => {
 });
 
 //====================to mark the attendance after qr scan============
-router.get('/mark-attendance/:name/:email/:company/:event/?', async (req, res) => {
+router.get('/mark-attendance/:name/:email/:company/:event', async (req, res) => {
   
      const { name, email, company, event } = req.params;
 
